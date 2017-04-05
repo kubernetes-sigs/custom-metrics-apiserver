5a2947500530a88625e7b4c90050ce9b3b3e0efe
Solly Ross <sross@redhat.com>
2017-04-04 21:59:51 -0400

    Remove dependency on modified k8s.io/apiserver
    Previously, using this repo required using a modified version
    of k8s.io/apiserver.  This commit removes that dependency, opting
    instead to include a stripped-down copy of the API server installer
    that specifically serves the needs of custom metrics API servers.
    
    This also fixes a bug in which certain endpoints were not always
    returning MetricValueLists.

--- pkg/apiserver/cmapis.go
+++ pkg/apiserver/cmapis.go
@@ -17,17 +17,17 @@ limitations under the License.
 package apiserver
 
 import (
+	"k8s.io/apimachinery/pkg/apimachinery"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/apimachinery"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apiserver/pkg/registry/rest"
-	genericapiserver "k8s.io/apiserver/pkg/server"
 	genericapi "k8s.io/apiserver/pkg/endpoints"
+	genericapiserver "k8s.io/apiserver/pkg/server"
 
-	"k8s.io/metrics/pkg/apis/custom_metrics"
-	metricstorage "k8s.io/custom-metrics-boilerplate/pkg/registry/custom_metrics"
+	specificapi "k8s.io/custom-metrics-boilerplate/pkg/apiserver/installer"
 	"k8s.io/custom-metrics-boilerplate/pkg/provider"
+	metricstorage "k8s.io/custom-metrics-boilerplate/pkg/registry/custom_metrics"
+	"k8s.io/metrics/pkg/apis/custom_metrics"
 )
 
 func (s *CustomMetricsAdapterServer) InstallCustomMetricsAPI() error {
@@ -38,14 +38,14 @@ func (s *CustomMetricsAdapterServer) InstallCustomMetricsAPI() error {
 		GroupVersion: groupMeta.GroupVersion.String(),
 		Version:      groupMeta.GroupVersion.Version,
 	}
-    groupVersion := metav1.GroupVersionForDiscovery{
-        GroupVersion: groupMeta.GroupVersion.String(),
-        Version:      groupMeta.GroupVersion.Version,
-    }
+	groupVersion := metav1.GroupVersionForDiscovery{
+		GroupVersion: groupMeta.GroupVersion.String(),
+		Version:      groupMeta.GroupVersion.Version,
+	}
 	apiGroup := metav1.APIGroup{
-		Name:              groupMeta.GroupVersion.String(),
-		Versions:          []metav1.GroupVersionForDiscovery{groupVersion},
-		PreferredVersion:  preferredVersionForDiscovery,
+		Name:             groupMeta.GroupVersion.String(),
+		Versions:         []metav1.GroupVersionForDiscovery{groupVersion},
+		PreferredVersion: preferredVersionForDiscovery,
 	}
 
 	cmAPI := s.cmAPI(groupMeta, &groupMeta.GroupVersion)
@@ -54,41 +54,35 @@ func (s *CustomMetricsAdapterServer) InstallCustomMetricsAPI() error {
 		return err
 	}
 
-	path := genericapiserver.APIGroupPrefix+"/"+groupMeta.GroupVersion.Group
+	path := genericapiserver.APIGroupPrefix + "/" + groupMeta.GroupVersion.Group
 	s.GenericAPIServer.HandlerContainer.Add(genericapi.NewGroupWebService(s.GenericAPIServer.Serializer, path, apiGroup))
 
 	return nil
 }
-func (s *CustomMetricsAdapterServer) cmAPI(groupMeta *apimachinery.GroupMeta, groupVersion *schema.GroupVersion) *genericapi.APIGroupVersion {
+func (s *CustomMetricsAdapterServer) cmAPI(groupMeta *apimachinery.GroupMeta, groupVersion *schema.GroupVersion) *specificapi.MetricsAPIGroupVersion {
 	resourceStorage := metricstorage.NewREST(s.Provider)
 
-	storage := map[string]rest.Storage{
-		// TODO: make this non-returning storage
-		"*": resourceStorage,
-		"*/*": resourceStorage,
-	}
+	return &specificapi.MetricsAPIGroupVersion{
+		DynamicStorage: resourceStorage,
+		APIGroupVersion: &genericapi.APIGroupVersion{
+			Root:         genericapiserver.APIGroupPrefix,
+			GroupVersion: *groupVersion,
 
-	return &genericapi.APIGroupVersion{
-		Root: genericapiserver.APIGroupPrefix,
-		GroupVersion: *groupVersion,
+			ParameterCodec:  metav1.ParameterCodec,
+			Serializer:      Codecs,
+			Creater:         Scheme,
+			Convertor:       Scheme,
+			UnsafeConvertor: runtime.UnsafeObjectConvertor(Scheme),
+			Copier:          Scheme,
+			Typer:           Scheme,
+			Linker:          groupMeta.SelfLinker,
+			Mapper:          groupMeta.RESTMapper,
 
-		ParameterCodec: metav1.ParameterCodec,
-		Serializer: Codecs,
-		Creater: Scheme,
-		Convertor: Scheme,
-		UnsafeConvertor: runtime.UnsafeObjectConvertor(Scheme),
-		Copier: Scheme,
-		Typer: Scheme,
-		SubresourceGroupVersionKind: nil, // TODO: do we need this?
-		Linker: groupMeta.SelfLinker,
-		Mapper: groupMeta.RESTMapper,
-		Storage: storage,
+			Context:                s.GenericAPIServer.RequestContextMapper(),
+			MinRequestTimeout:      s.GenericAPIServer.MinRequestTimeout(),
+			OptionsExternalVersion: &schema.GroupVersion{Version: "v1"},
 
-		// TODO: Admit?
-		Context: s.GenericAPIServer.RequestContextMapper(),
-		MinRequestTimeout: s.GenericAPIServer.MinRequestTimeout(),
-		OptionsExternalVersion: &schema.GroupVersion{Version: "v1"},
-
-		ResourceLister: provider.NewResourceLister(s.Provider),
+			ResourceLister: provider.NewResourceLister(s.Provider),
+		},
 	}
 }
